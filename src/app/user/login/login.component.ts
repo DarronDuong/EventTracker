b099814495699bcdd7a388eb9def37b0eca68539
Trung Duong <trungd@accelrelation.com.au>
2019-04-17 10:16:57 +1000

    return when cancel/login

--- src/app/user/login/login.component.ts
+++ src/app/user/login/login.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -10,12 +11,17 @@ export class LoginComponent implements OnInit {
 
   password;
   userName;
-  constructor(private auth: AuthService) { }
+  constructor(private auth: AuthService, private router: Router) { }
 
   ngOnInit() {
   }
 
   login(formValues){
     this.auth.loginUser(formValues.userName, formValues.password);
+    this.router.navigate(['events']);
+  }
+
+  cancel(){
+    this.router.navigate(['events']);
   }
 }
